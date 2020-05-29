a3f015a549428249a825a0d9a0be037f30e0ab20
Jérémy Choisy <choisy.jeremy@gmail.com>
2020-05-29 17:14:35 +0200

    CSVLoader + remote server

--- src/main/java/Client/OrientDBClient.java
+++ src/main/java/Client/OrientDBClient.java
@@ -1,16 +1,27 @@
 package Client;
 
+import Loaders.CSVLoader;
 import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.ODatabaseType;
 import com.orientechnologies.orient.core.db.OrientDB;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OType;
 
 public class OrientDBClient {
     private OrientDB orient;
     private ODatabaseSession db;
+    private CSVLoader csvLoader;
+    private static final String[] VERTEX_CLASSES = {"Person", "Feedback", "Order", "Tag", "Post", "Invoice", "Seller", "Product"};
 
     public OrientDBClient(){
-        this.orient = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig());
-        this.db = this.orient.open("big-data-project", "root", "root");
+        this.orient = new OrientDB("remote:54.93.113.62", "root", "root", OrientDBConfig.defaultConfig());
+//        this.orient.drop("big-data-project");
+        this.orient.create("big-data-project", ODatabaseType.PLOCAL);
+        this.db = this.orient.open("big-data-project", "admin", "admin");
+        this.csvLoader = new CSVLoader(this.db);
+        createSchema();
+//        importData();
     }
 
     public void close(){
@@ -20,5 +31,23 @@ public class OrientDBClient {
 
     public ODatabaseSession getDB() {
         return this.db;
+    }
+
+    private void createSchema() {
+        for (String vertexClass : VERTEX_CLASSES) {
+            if (db.getClass(vertexClass) == null) {
+                db.createVertexClass(vertexClass);
+            }
+        }
+
+        if (db.getClass("Person").getProperty("id") == null) {
+            db.getClass("Person").createProperty("id", OType.STRING);
+            db.getClass("Person").createIndex("Person_id_index", OClass.INDEX_TYPE.UNIQUE, "id");
+        }
+    }
+
+    private void importData() {
+        this.csvLoader.loadCSVIntoDB("person.csv", "\\|", "Person");
+        this.csvLoader.loadCSVIntoDB("feedback.csv", "\\|", "Feedback");
     }
 }
