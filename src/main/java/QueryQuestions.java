1d63717774d3a6642e66dc43ec6d2d616e38e0ad
DSGAndre <andredasilvagoncalves@hotmail.com>
2020-06-01 13:39:16 +0200

    change edge

--- src/main/java/QueryQuestions.java
+++ src/main/java/QueryQuestions.java
@@ -36,15 +36,14 @@ class QueryQuestions {
      and return the tag which he/she has engaged the greatest times in the posts.
     */
 
-    // A refaire !!!!!
     void question1(String clientId) {
-        query = "Select $profile,$posts,$brandList2,$feedback,$orders,$brandList1 "
-                + "let $profile=(select from `Customer` where id=?),"
-                + "$posts= (select Out(\'PersonHasPost\') from `Customer` where id=?),"
-                + "$brandList2=(select pid, count(pid) from (select Out(\'PersonHasPost\').Out(\'PostHasTag\').productId as pid from `Customer` where id=? unwind pid) group by pid order by count Desc),"
-                + "$feedback=(select Expand(Feedback) from `Customer` where id=?),"
-                + "$orders=(select Expand(Order) from `Customer` where id=?),"
-                + "$brandList1= (select list.brand as brand, count(list.brand) as cnt from (select Order.Orderline as list from `Customer` where id=? unwind list) group by list.brand ORDER BY cnt DESC)";
+        query = "Select $client,$orders,$feedback,$posts,$list1,$list2 "
+                + "let $client=(select from `Person` where id=?),"
+                + "$orders=(select Expand(Order) from `Person` where id=?),"
+                + "$feedback=(select Expand(Feedback) from `Person` where id=?),"
+                + "$posts= (select Out(\'PersonGaveFeedback\') from `Person` where id=?),"
+                + "$list1= (select list.Vendor as brand, count(list.Vendor) as cnt from (select Order.Orderline as list from `Person` where id=? unwind list) group by list.Vendor ORDER BY cnt DESC),"
+                + "$list2=(select pid, count(pid) from (select Out(\'PersonHasPost\').Out(\'PostHasTag\').productId as pid from `Customer` where id=? unwind pid) group by pid order by count Desc)";
 
         OResultSet rs = db.query(query, clientId, clientId, clientId, clientId, clientId, clientId);
 
@@ -59,11 +58,9 @@ class QueryQuestions {
 
     /* For a given product during a given period, find the people who commented or posted on it, and had bought it. */
 
-    // Normalement bon !!!!
-
     void question2(String productId, String year) {
-        query = "Select $person let $list=(select In(\'PostHasTag\').In(\'PersonHasPost\').id as pid "
-                + "from `Product` where productId=?),$person=(select PersonId,Orderline.productId from Order "
+        query = "Select $client let $list=(select In(\'PostIsLabelledByTag \').In(\'PostIsLabelledByTag \').id as clientId "
+                + "from `Vendor` where rid=?),$client =(select PersonId,Orderline.productId from Order "
                 + "where OrderDate> ? and PersonId in $list and ? in Orderline.productId)";
 
         OResultSet rs = db.query(query, productId, year, productId);
@@ -87,7 +84,7 @@ class QueryQuestions {
     // A Modifier avec la période
     void question3(String productId) {
         query = "Select $post,$feedback "
-                + "let $post=(select Expand(In(\'PostHasTag\')) from `Product` "
+                + "let $post=(select Expand(In(\'PersonHasInterestInTag \')) from `Product` "
                 + "where productId=?),"
                 + "$feedback=(select * from `Feedback` where asin=? and feedback.charAt(1).asInteger() <3)";
         OResultSet rs = db.query(query, productId, productId);
@@ -104,12 +101,11 @@ class QueryQuestions {
     and finally return the common friends of these two persons.
      */
 
-    //Pas bon
     void question4() {
         String query = "SELECT commonset.size() from (SELECT intersect($set1,$set2) as commonset "
-                + "let $person = (select pid from (select PersonId as pid, SUM(TotalPrice) as sum from Order Group by PersonId order by sum desc limit 2)),"
-                + "$set1=(TRAVERSE out(\"Knows\") FROM (select from Customer where PersonId=$person.pid[0]) while $depth <= 3 STRATEGY BREADTH_FIRST),"
-                + "$set2=(TRAVERSE out(\"Knows\") FROM (select from Customer where PersonId=$person.pid[1]) while $depth <= 3 STRATEGY BREADTH_FIRST))";
+                + "let $person = (select clientId from (select PersonId as clientId, SUM(TotalPrice) as sum from Order Group by PersonId order by sum desc limit 2)),"
+                + "$set1=(TRAVERSE out(\"PersonKnowsPerson \") FROM (select from Person where PersonId=$person.pid[0]) while $depth <= 3 STRATEGY BREADTH_FIRST),"
+                + "$set2=(TRAVERSE out(\"PersonKnowsPerson \") FROM (select from Person where PersonId=$person.pid[1]) while $depth <= 3 STRATEGY BREADTH_FIRST))";
         OResultSet rs = db.query(query);
 
         while (rs.hasNext()) {
@@ -125,11 +121,11 @@ class QueryQuestions {
      Finally, return feedback with the 5-rating review of those bought products.
      */
 
-    //  Pas bon
     void question5(String clientId, String marque) {
-        String query = "Select Out(\'PersonHasPost\').Out(\'PostHasTag\') " +
-                "as tags from (select Expand(Out(\'Knows\')) from Customer where id=?) " +
-                "Where ? in Order.Orderline.brand unwind tags";
+        String query = "Select Out(\'PersonHasPost\').Out(\'PostIsLabelledByTag\') " +
+                "as tags from (select Expand(Out(\'Knows\')) from Person where id=?) " +
+                "Where ? in Order.Orderline.brand unwind tags," +
+                "$feedback=(select * from `Feedback` where feedback.charAt(1).asInteger() ==5)";
 
         OResultSet rs = db.query(query, clientId, marque);
 
@@ -147,12 +143,11 @@ class QueryQuestions {
     and return the TOP 3 best sellers from all these persons' purchases.
      */
 
-    //  Pas bon c'est pas un best of 3
     void question6(String clientId1, String clientId2) {
         query = "SELECT transactions, count(transactions) as cnt "
                 + "FROM(SELECT Order.Orderline.productId as transactions from(SELECT EXPAND(path) from(SELECT shortestPath($from, $to) AS path "
-                + "LET $from = (SELECT FROM Customer WHERE id=?),"
-                + "$to = (SELECT FROM Customer WHERE id=?))) unwind transactions) GROUP BY transactions Order by cnt DESC LIMIT 5";
+                + "LET $from = (SELECT FROM Person WHERE id=?),"
+                + "$to = (SELECT FROM Person WHERE id=?))) unwind transactions) GROUP BY transactions Order by cnt DESC LIMIT 3";
         OResultSet rs = db.query(query, clientId1, clientId2);
 
         while (rs.hasNext()) {
@@ -169,27 +164,8 @@ class QueryQuestions {
      analyze the reviews for these items to see if there are any negative sentiments.
      */
 
-    // Rien à voir
     void question7(String brand) {
-        query = "Select feedback from Feedback where asin in "
-                + "(Select dlist from(Select set(dlist) as dlist  "
-                + "from(Select $declineList.asin as dlist "
-                + "let $list1 = (Select asin,count(asin) as cnt from "
-                + "(Select ol_unwind.asin as asin, ol_unwind.brand as brand from "
-                + "(Select Orderline as ol_unwind from (Select From Order Where OrderDate>\"2018\" and OrderDate<\"2019\" and ? in Orderline.brand) unwind ol_unwind)) "
-                + "where brand=? group by asin order by cnt DESC), "
-                + "$list2=(Select asin,count(asin) as cnt from (Select ol_unwind.asin as asin, ol_unwind.brand as brand from "
-                + "(Select Orderline as ol_unwind from (Select From Order Where OrderDate>\"2019\" and OrderDate<\"2020\" and ? in Orderline.brand) unwind ol_unwind)) "
-                + "where brand=? group by asin order by cnt DESC), $declineList=compareList($list1,$list2))))";
-        OResultSet rs = db.query(query, brand, brand, brand, brand);
-
-        while (rs.hasNext()) {
-            OResult item = rs.next();
-            System.out.println(item);
-        }
-
-        rs.close();
-        orient.close();
+        //TODO problème on a pas réussi à anlyse the review avec le sentiments négatif. On aurai put prendre en compte la note mais ca ne représente pas forcément un avis négatif
     }
 
     /*
@@ -198,12 +174,13 @@ class QueryQuestions {
      */
 
     // Ajoutez en fonction d'une date
-    void question8() {
+    void question8(String year) {
         query = "Select Sum(Popularity) from(Select In(\'PostHasTag\').size() as Popularity "
                 + "from `Product` Where productId in (Select  Distinct(Orderline.productId) "
                 + "From (Select Orderline From Order let  $brand=(select name as brand from `Vendor` where country='China') "
-                + "Where OrderDate>\"2018\" and OrderDate<\"2019\" unwind Orderline) Where Orderline.brand in $brand.brand))";
-        OResultSet rs = db.query(query);
+                + "Where OrderDate>? and OrderDate<? unwind Orderline) Where Orderline.brand in $brand.brand))";
+        int year2 = Integer.parseInt(year) + 1;
+        OResultSet rs = db.query(query, year, String.valueOf(year2));
 
         while (rs.hasNext()) {
             OResult item = rs.next();
@@ -218,10 +195,9 @@ class QueryQuestions {
     compare the number of the male and female customers, and return the most recent posts of them.
      */
 
-    //Pas bon
     void question9() {
         query = "Select Orderline.brand, count(*) from(Select PersonId, Orderline From Order "
-                + "Let  $brand=(select name as brand from `Vendor` where country='China') Where OrderDate>\"2018\" and OrderDate<\"2019\" unwind Orderline) "
+                + "Let  $brand=(select name as brand from `Vendor` where country='France') Where OrderDate>\"2017\" and OrderDate<\"2018\" unwind Orderline) "
                 + "Where Orderline.brand in $brand.brand Group by Orderline.brand Order by count DESC LIMIT 3";
         OResultSet rs = db.query(query);
         while (rs.hasNext()) {
@@ -239,11 +215,10 @@ class QueryQuestions {
      and return their recent reviews and tags of interest.
      */
 
-    // Je crois que c'est bon
     void question10() {
-        query = "SELECT id, max(Order.OrderDate) as Recency,Order.size() as Frequency,sum(Order.TotalPrice) as Monetary FROM Customer "
-                + "Where id in(Select id, count(id) as cnt from (Select IN(\'PersonHasPost\').id[0] as id From Post "
-                + "Where creationDate>= date( \'2012-10-01\', \'yyyy-MM-dd\')) Group by id  Order by cnt DESC limit 10) GROUP BY id";
+        query = "SELECT id, max(Order.OrderDate) as Recency,Order.size() as Frequency,sum(Order.TotalPrice) as Monetary FROM Person "
+                + "Where id in(Select id, count(id) as cnt from (Select IN(\'PersonGaveFeedback\').id[0] as id From Post "
+                + "Where creationDate>= date( \'2012-04-08\', \'yyyy-MM-dd\')) Group by id  Order by cnt DESC limit 10) GROUP BY id";
         OResultSet rs = db.query(query);
         while (rs.hasNext()) {
             OResult item = rs.next();
